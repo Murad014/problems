9366910b8b1d8b925ced6da1073c19d9cfc1822f
Murad R. Guliyev <murad.guliyev@modenis.local>
2022-04-20 14:05:07 +0400

    A Fancy Fence - Python CodeForces

---
+++ CodeForces/A. Fancy Fence.py
@@ -0,0 +1,20 @@
+
+
+pols = []
+
+for i in range(3, 1000):
+	ans = ((i-2) * 180) / i
+	if ans == int(ans):
+		pols.append(int(ans))
+
+#print(pols)
+
+t = int(input())
+for _ in range(t):
+	a = int(input())
+	
+	if a in pols:
+		print("YES")
+	else:
+		print("NO")
+
