81ba1c64afa02f70bb52cef8724f3501befaa50f
Murad R. Guliyev <murad.guliyev@modenis.local>
2022-05-23 16:39:54 +0400

    1428C - ABBB

---
+++ CodeForces/1428C_ABBB.py
@@ -0,0 +1,26 @@
+
+
+def solve(s):
+	stack = [s[0]]
+
+	for i in range(1, len(s)):
+
+		if len(stack) != 0:
+			if stack[len(stack) - 1] + s[i] == "AB" or stack[len(stack) - 1] + s[i] == "BB":
+				stack.pop()
+
+			else:
+				stack.append(s[i])
+
+		else:
+			stack.append(s[i])
+
+	print(len(stack))
+
+
+
+t = int(input())
+
+for _ in range(t):
+	s = input()
+	solve(s)
